1bc5a17d3c3b430c48a4932bf247e619a153b7a7
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-05-31 23:19:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
-// import Home from "./pages/Home/Home";
-// import VideosPs from "./pages/VideosPs/VideosPs";
-// import HomePosLogin from "./pages/HomePosLogin/HomePosLogin";
+import Home from "./pages/Home/Home";
 import HomePosLogin from "./pages/HomePosLogin/HomePosLogin"
 
 function App() {
   return (
     <>
-      {/* <Home cadastros="8" /> */}
-      {/* <VideosPs use="notVisible"/> */}
-      <HomePosLogin />
+      <Home cadastros="8" />
     </>
   );
 }
